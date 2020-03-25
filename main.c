1cb118fde6a153fd820accb7598855df6cbc3b64
EL_abbas Salah Hatata <abassalah92@gmail.com>
2020-03-25 22:27:41 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#define max 100
+void print(int a[][max], int size)  
+{ 
+for (int i = 0; i < size; i++) { 
+    for (int j = 0; j < size; j++) { 
+    printf("%d", a[i][j]); 
+    } 
+     printf("\n");
+} 
+}
+void innerPattern(int n) { 
+      
+// Pattern Size 
+int size = 2 * n - 1; 
+int front = 0; 
+int back = size - 1; 
+int a[max][max]; 
+while (n != 0)  
+{ 
+    for (int i = front; i <= back; i++) { 
+    for (int j = front; j <= back; j++) { 
+        if (i == front || i == back || 
+            j == front || j == back) 
+        a[i][j] = n; 
+    } 
+    } 
+    ++front; 
+    --back; 
+    --n; 
+} 
+print(a, size); 
+} 
+int main(void) {
+  
+   // Input 
+  int R_num=4;    
+      
+    // function calling 
+    innerPattern(R_num);
+  return 0;
+}
